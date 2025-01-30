ec5f6885f8d293b38bebc9917dd64b9b15ee9fb0
alexsal2025 <alexsal2024@ya.ru>
2025-01-30 18:56:45 +0300

    Add files via upload

---
+++ SetMyCommands/SetMyCommands.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+
+#define WIFI_SSID "сеть"
+#define WIFI_PASSWORD "пароль"
+#define BOT_TOKEN "токен бота Botfather"
+
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
+
+unsigned long bot_lasttime; // last time messages' scan has been done
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+void handleNewMessages(int numNewMessages)
+{
+  Serial.print("handleNewMessages ");
+  Serial.println(numNewMessages);
+  
+  String answer;
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    telegramMessage &msg = bot.messages[i];
+    Serial.println("Received " + msg.text);
+    if (msg.text == "/help")
+      answer = "Помощь";
+    else if (msg.text == "/start")
+      answer = "Привет, мир";
+    else
+      answer = "";//если незапрограммированное сообщение
+
+    bot.sendMessage(msg.chat_id, answer, "Markdown");
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org");
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+
+  bot_setup();
+}
+
+void loop()
+{
+  if (millis() - bot_lasttime > BOT_MTBS)
+  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages)
+    {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    bot_lasttime = millis();
+  }
+}
